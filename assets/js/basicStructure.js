a4ba1e985f80bb6d3ac0d3dd5b6b4f728e607957
Muhammad Ashraf <muashrafalii@gmail.com>
2022-03-18 17:55:16 +0200

    added loader

--- assets/js/basicStructure.js
+++ assets/js/basicStructure.js
@@ -3,7 +3,8 @@ const navBar = document.querySelector("#navBar"),
   navBarShower = document.querySelector("#header .traverseDown"),
   navBarHidder = document.querySelector("#navBar .navUpIcon"),
   mainContainer = document.querySelector("main"),
-  header = document.querySelector("#header");
+  header = document.querySelector("#header"),
+  loader = document.querySelector("#loader");
 
 // Event Listners
 navBarShower.addEventListener("click", () => navBar.classList.add("active"));
@@ -15,3 +16,5 @@ mainContainer.addEventListener("scroll", (e) => {
     header.classList.remove("scrolled");
   }
 });
+
+window.addEventListener("load", () => loader.classList.add("hidden"));
