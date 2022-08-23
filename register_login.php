65a976fdd1bacf31711c33d2b08b77bcaa2d4d24
Muhammad Ashraf <muashrafalii@gmail.com>
2022-08-23 16:49:47 +0200

    added login/register JS

--- register_login.php
+++ register_login.php
@@ -9,6 +9,7 @@
       href="./assets/imgs/logo.png"
       type="image/x-icon"
     />
+    <script src="./assets/js/register_login.js" defer></script>
     <link rel="stylesheet" href="./assets/css/register_login.css">
     <title>Create an Account -- Mussika</title>
   </head>
